7ad4c7027ba81a8f74562cfeb9c6202e900661d7
Md Ariful Islam <m.ariful@graduate.utm.my>
2022-11-26 22:21:43 +0800

    Angular Interpolation

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
+  name: string = 'Clean Code';
+
+  author: string = 'Robert C Martin';
+
+  src: string = 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg';
+
+  name2: string ='Software Architect';
+
+  author2: string = 'Anatoly Volkhover';
+
+  src2: string = 'https://m.media-amazon.com/images/P/1697271065.01._SCLZZZZZZZ_SX500_.jpg';
+
   constructor() { }
 
   ngOnInit(): void {
